00d0cdce583f6e3466cca43d831c3817a9411bab
Gary Murphy <compsciguy.it@gmail.com>
2019-06-05 13:29:22 +1000

    Refactored by creating a test setup function

--- SupermarketKata/test/checkoutTests.js
+++ SupermarketKata/test/checkoutTests.js
@@ -1,13 +1,16 @@
 const Checkout = require('../checkout.js');
 var expect = require('chai').expect;
+var checkout;
+
+beforeEach(function(){
+    checkout = new Checkout();
+});
 
 it('Can add item price', function(){
-    var checkout = new Checkout();
     checkout.addItemPrice('a', 1);
 });
 
 it('Can add item', function(){
-    var checkout = new Checkout();
     checkout.addItemPrice('a', 1);
     checkout.addItem('a');
 });
